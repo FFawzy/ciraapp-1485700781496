35826507e12fc26a78989947d92a3461fd941a9d
Fady Fawzy <fafawzy@eg.ibm.com>
2017-01-29 09:14:54 -0600

    KJ

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -1,4 +1,5 @@
 <?php
+require_once APPPATH.'libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
 class Contact extends REST_Controller
@@ -15,13 +16,13 @@ class Contact extends REST_Controller
      $this->load->model('Contact_m');
      $Data;
      if($this->post('name')!=null){
-     	$Data['name']=$this->post('name');
+      $Data['name']=$this->post('name');
      } if($this->post('email')!=null){
-     	$Data['email']=$this->post('email');
+      $Data['email']=$this->post('email');
      } if($this->post('phone')!=null){
-     	$Data['phone']=$this->post('phone');
+      $Data['phone']=$this->post('phone');
      }
-      				date_default_timezone_set("America/Chicago");
+              date_default_timezone_set("America/Chicago");
                     $tempdate = getdate();
                     $strdate = $tempdate['year']."-".$tempdate['mon']."-".$tempdate['mday']." ".$tempdate['hours'].":".$tempdate['minutes'].":".$tempdate['seconds'];
                     $Data['date_created'] = $strdate;
@@ -33,13 +34,13 @@ class Contact extends REST_Controller
      $this->load->model('Contact_m');
      $Data;
      if($this->put('name')!=null){
-     	$Data['name']=$this->put('name');
+      $Data['name']=$this->put('name');
      } if($this->post('email')!=null){
-     	$Data['email']=$this->put('email');
+      $Data['email']=$this->put('email');
      } if($this->post('phone')!=null){
-     	$Data['phone']=$this->put('phone');
+      $Data['phone']=$this->put('phone');
      }
-      				
+              
      $contacts=$this->Contact_m->update($Data,array('id'=>$id));
      $this->response('Contact Updated', 200);
   }
@@ -47,7 +48,7 @@ class Contact extends REST_Controller
   {
      $this->load->model('Contact_m');
     
-      				
+              
      $contacts=$this->Contact_m->delete($id);
      $this->response('Contact Deleted', 200);
   }
