ad7331a5df8597d10ba9c5755aefdbe3549b16cb
Fady Fawzy <fafawzy@eg.ibm.com>
2017-01-31 06:31:57 -0600

    aklsdklad

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -4,16 +4,22 @@ use Restserver\Libraries\REST_Controller;
 
 class Contact extends REST_Controller
 {
+  parent::__construct();        
+          $this->load->model('Contact_m');
+     $this->load->model('Phone_m');
+     $this->load->model('Email_m');
+    }
   public function index_get()
   {
-     $this->load->model('Contact_m');
+    
      $contacts=$this->Contact_m->get();
      $this->response($contacts, 200);
   }
+  
 
   public function index_post()
   {
-     $this->load->model('Contact_m');
+     
      $Data;
      if($this->post('name')!=null){
       $Data['name']=$this->post('name');
@@ -31,7 +37,7 @@ class Contact extends REST_Controller
   }
   public function index_put($id)
   {
-     $this->load->model('Contact_m');
+     
      $Data;
      if($this->put('name')!=null){
       $Data['name']=$this->put('name');
@@ -44,9 +50,38 @@ class Contact extends REST_Controller
      $contacts=$this->Contact_m->update($Data,array('id'=>$id));
      $this->response('Contact Updated', 200);
   }
+  public function match($contact){
+    
+     $contacts=$this->Contact_m->get(null,array('name'=>$contact->name));
+     if(count($contacts)>0){
+        if($contacts[0]->phone!=$contact->phone){
+           
+            $Data['contact_id']=$contacts[0]->id;
+            $Data['phone']=$contact->phone;
+           date_default_timezone_set("America/Chicago");
+                    $tempdate = getdate();
+                    $strdate = $tempdate['year']."-".$tempdate['mon']."-".$tempdate['mday']." ".$tempdate['hours'].":".$tempdate['minutes'].":".$tempdate['seconds'];
+                    $Data['date_created'] = $strdate;
+                   $this->Phone_m->save($Data);
+
+        }
+        if($contacts[0]->email!=$contact->email){
+           
+            $Data['contact_id']=$contacts[0]->id;
+            $Data['phone']=$contact->email;
+           date_default_timezone_set("America/Chicago");
+                    $tempdate = getdate();
+                    $strdate = $tempdate['year']."-".$tempdate['mon']."-".$tempdate['mday']." ".$tempdate['hours'].":".$tempdate['minutes'].":".$tempdate['seconds'];
+                    $Data['date_created'] = $strdate;
+                   $this->Email_m->save($Data);
+
+        }
+     }
+
+  }
   public function index_delete($id)
   {
-     $this->load->model('Contact_m');
+    
     
               
      $contacts=$this->Contact_m->delete($id);
---
+++ application/models/Phone_m.php
@@ -0,0 +1,76 @@
+<?php
+class Phone_m extends CI_Model{
+    function __construct() {
+        parent::__construct();
+       
+    }
+
+  
+    public $_rules_add = array(
+        
+        'contact_id'=> array(
+            'field' =>'contact_id', 
+            'label'=> 'contact_id', 
+            'rules'=>'trim|required'
+           )
+        
+        
+       
+    );
+    
+    public $_rules_edit = array(
+     'contact_id'=> array(
+            'field' =>'contact_id', 
+            'label'=> 'contact_id', 
+            'rules'=>'trim|required'
+           )
+        
+    );
+    
+  
+    
+    function get($select_str = null, $condition_arr = null, $ordercol = null, $orderoption = null, $limit = null, $start = null) {
+    
+        if ($select_str != null) {
+            $this -> db -> select($select_str);
+        }
+        if ($condition_arr != null) {
+            $this -> db -> where($condition_arr);
+        }
+        if ($limit != null) {
+            $this -> db -> limit($limit, $start);
+        }
+        //$this -> db -> order_by('membership_type','asc');
+    
+        $this -> db -> from('phone');
+        //print_r($this -> db);
+        return $this -> db -> get() -> result();
+    }
+   
+
+    function save($User_arr) {
+        
+        return $this -> db -> insert('phone', $User_arr);
+
+    }
+    function count(){
+        $this -> db -> from('phone');
+        $items = $this -> db -> get() -> result();
+        return count($items);
+    }
+    
+    function update($data, $condition) {
+        
+        
+        
+        return $this -> db -> update('phone', $data, $condition);
+    
+    }
+    function delete($id) {
+        $arr = array('id'=>$id);
+        
+        $this -> db -> where($arr) -> delete('phone');
+    }
+    
+  
+}
